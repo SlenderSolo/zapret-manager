987cb36a1c6a9c0f78b429fbc020c72094d58f0c
SlenderSolo <55142604+SlenderSolo@users.noreply.github.com>
2025-06-29 05:54:15 +0300

    changed presets

--- presset_multid_m.cmd
+++ presset_multid_m.cmd
@@ -2,8 +2,8 @@ set "BIN=%~dp0bin\"
 set "LISTS=%~dp0lists\"
 
 start "zapret: http,https,quic" /min "%BIN%winws.exe" --wf-tcp=80,443 --wf-udp=443,50000-50099 ^
---filter-tcp=80 --hostlist="%LISTS%list-general.txt"  --dpi-desync=fake,fakedsplit --dpi-desync-fooling=badseq --dpi-desync-split-pos=midsld --new ^
---filter-tcp=443 --hostlist="%LISTS%list-youtube.txt" --dpi-desync=fake,multidisorder --dpi-desync-fooling=md5sig --dpi-desync-split-pos=1,midsld --dpi-desync-fake-tls="%~dp0bin\tls_clienthello_www_google_com.bin" --dpi-desync-repeats=11 --new ^
+--filter-tcp=80 --hostlist="%LISTS%list-general.txt" --dpi-desync=fake,fakedsplit --dpi-desync-fooling=badseq --dpi-desync-split-pos=midsld --new ^
+--filter-tcp=443 --hostlist="%LISTS%list-youtube.txt" --dpi-desync=fake,multidisorder --dpi-desync-fooling=md5sig --dpi-desync-split-pos=1,midsld --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --dpi-desync-repeats=11 --new ^
 --filter-tcp=443 --hostlist="%LISTS%list-general.txt" --dpi-desync=fake,multidisorder --dpi-desync-fooling=md5sig --dpi-desync-split-pos=1,midsld --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.yandex.ru --dpi-desync-repeats=6 --new ^
 --filter-tcp=443 --ipset="%LISTS%ipset_all.txt" --dpi-desync=fake,multidisorder --dpi-desync-fooling=md5sig --dpi-desync-split-pos=1,midsld --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.yandex.ru --dpi-desync-repeats=6 --new ^
 --filter-udp=443 --ipset="%LISTS%ipset_all.txt" --dpi-desync=fake --dpi-desync-repeats=11 --new ^
--- presset_multis_ttl.cmd
+++ presset_multis_ttl.cmd
@@ -2,7 +2,7 @@ set "BIN=%~dp0bin\"
 set "LISTS=%~dp0lists\"
 
 start "zapret: http,https,quic" /min "%BIN%winws.exe" --wf-tcp=80,443 --wf-udp=443,50000-50099 ^
---filter-tcp=80 --hostlist="%LISTS%list-general.txt"  --dpi-desync=fake,fakedsplit --dpi-desync-fooling=badseq --dpi-desync-split-pos=midsld --new ^
+--filter-tcp=80 --hostlist="%LISTS%list-general.txt" --dpi-desync=fake,fakedsplit --dpi-desync-fooling=badseq --dpi-desync-split-pos=midsld --new ^
 --filter-tcp=443 --hostlist="%LISTS%list-youtube.txt" --dpi-desync=fake,multisplit --dpi-desync-ttl=1 --dpi-desync-autottl=-1 --dpi-desync-split-pos=1 --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --dpi-desync-repeats=11 --new ^
 --filter-tcp=443 --hostlist="%LISTS%list-general.txt" --dpi-desync=fake,multisplit --dpi-desync-ttl=1 --dpi-desync-autottl=-1 --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=6 --new ^
 --filter-tcp=443 --ipset="%LISTS%ipset_all.txt" --dpi-desync=fake,multisplit --dpi-desync-ttl=1 --dpi-desync-autottl=-1 --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=6 --new ^
