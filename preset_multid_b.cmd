1d0ec0eb7c56c7c411b07d6debd994d412ba6045
SlenderSolo <55142604+SlenderSolo@users.noreply.github.com>
2025-08-01 21:50:21 +0300

    change ipset name

--- preset_multid_b.cmd
+++ preset_multid_b.cmd
@@ -5,8 +5,8 @@ start "zapret: http,https,quic" /min "%BIN%winws.exe" --wf-tcp=80,443 --wf-udp=4
 --filter-tcp=80 --hostlist="%LISTS%list-general.txt" --dpi-desync=fake,fakedsplit --dpi-desync-split-pos=midsld --dpi-desync-fooling=badseq --new ^
 --filter-tcp=443 --hostlist="%LISTS%list-youtube.txt" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=11 --dpi-desync-fooling=badseq --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --new ^
 --filter-tcp=443 --hostlist="%LISTS%list-general.txt" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.yandex.ru --new ^
---filter-tcp=443 --ipset="%LISTS%ipset_all.txt" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.yandex.ru --new ^
---filter-udp=443 --ipset="%LISTS%ipset_all.txt" --dpi-desync=fake --dpi-desync-repeats=11 --new ^
---filter-udp=443 --hostlist="%LISTS%list-youtube.txt" --dpi-desync=fake --dpi-desync-repeats=20 --dpi-desync-fake-quic="%~dp0bin\quic_initial_www_google_com.bin" --new ^
+--filter-tcp=443 --ipset="%LISTS%ipset-all.txt" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.yandex.ru --new ^
+--filter-udp=443 --ipset="%LISTS%ipset-all.txt" --dpi-desync=fake --dpi-desync-repeats=11 --new ^
+--filter-udp=443 --hostlist="%LISTS%list-youtube.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="%~dp0bin\quic_initial_www_google_com.bin" --new ^
 --filter-udp=443 --hostlist="%LISTS%list-general.txt" --dpi-desync=fake --dpi-desync-repeats=11 --new ^
 --filter-udp=50000-50099 --filter-l7=discord,stun --dpi-desync=fake
