47553ab874e25f4a436e1393de52c304ce221824
SlenderSolo <55142604+SlenderSolo@users.noreply.github.com>
2025-10-08 06:12:07 +0300

    Update and rename tool_enable_timestamps.cmd to tool_enable_timestamps_and_ECN.cmd

--- tool_enable_timestamps_and_ECN.cmd
+++ tool_enable_timestamps_and_ECN.cmd
@@ -1,7 +1,8 @@
 @echo off
 
 echo Administrator rights are required
-echo enable tcp timestamps
+echo enable tcp timestamps and ECN
 pause
 netsh interface tcp set global timestamps=enabled
+netsh int tcp set global ecn=enabled
 pause
