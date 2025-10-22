a013f876084b8b04e929cb2e2f1c642e8cfa3df2
SlenderSolo <55142604+SlenderSolo@users.noreply.github.com>
2025-10-23 01:16:52 +0300

    update cloud

--- preset_hostfakesplit.cmd
+++ preset_hostfakesplit.cmd
@@ -10,7 +10,7 @@ start "zapret: http,https,quic" /min "%BIN%winws.exe" --wf-tcp=80,443 --wf-udp=4
 --filter-tcp=80 --hostlist="%LISTS%list-general.txt" --dpi-desync=fakedsplit --dpi-desync-fooling=badseq --dpi-desync-split-pos=midsld --new ^
 --filter-tcp=443 --hostlist="%LISTS%list-youtube.txt" --dpi-desync=hostfakesplit --dpi-desync-fooling=badseq --dpi-desync-hostfakesplit-mod=altorder=1 --dpi-desync-hostfakesplit-midhost=midsld --new ^
 --filter-tcp=443 --hostlist="%LISTS%list-general.txt" --dpi-desync=fake,hostfakesplit --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=0 --dpi-desync-fake-tls-mod=rnd,dupsid,sni=ya.ru --new ^
---filter-tcp=443 --ipset="%LISTS%ipset-all.txt" --dpi-desync=fake,hostfakesplit --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=0 --dpi-desync-fake-tls-mod=rnd,dupsid,sni=ya.ru --new ^
+--filter-tcp=443 --ipset="%LISTS%ipset-cloud.txt" --dpi-desync=fake,hostfakesplit --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=0 --dpi-desync-fake-tls-mod=rnd,dupsid,sni=ya.ru --new ^
 --filter-l7=quic --hostlist="%LISTS%list-youtube.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="%~dp0bin\quic_initial_www_google_com.bin" --new ^
 --filter-l7=quic --hostlist="%LISTS%list-general.txt" --dpi-desync=fake --dpi-desync-repeats=11 --new ^
 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6
